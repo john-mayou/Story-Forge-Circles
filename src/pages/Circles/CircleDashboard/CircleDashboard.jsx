ca3b1be5fa3ce57ac3b2a1e35805ff764697c8a5
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-04-12 15:08:58 -0500

    searchinputs added

--- src/pages/Circles/CircleDashboard/CircleDashboard.jsx
+++ src/pages/Circles/CircleDashboard/CircleDashboard.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function CircleDashboard() {
+  
+  const handleSearch = () => {
+   
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+     handleSearch();
+    }
+  };
+
   return (
     <main className="content-main">
-    <h1>CircleDashboard</h1>
+      <h1>CircleDashboard</h1>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search manuscript"
+        onChange={(e) => e.target.value}
+        onKeyPress={handleKeyPress}
+      />
+      <button className="search-btn" onClick={handleSearch}>
+        Search
+      </button>
     </main>
-
-  )
+  );
 }
