27f6574d031e19bbad87a4c81fdabe61537e372f
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-04-11 23:31:52 -0500

    added logic to join btn

--- src/components/CircleTableView.jsx
+++ src/components/CircleTableView.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function CircleTableView({ circlelist, isJoined = false }) {
+  // Get the current user's ID from the Redux store
+  const { id } = useSelector((store) => store.user);
+
   return (
     <table>
       <thead>
@@ -10,16 +14,20 @@ export default function CircleTableView({ circlelist, isJoined = false }) {
         </tr>
       </thead>
       <tbody>
-        {circlelist.map((circle) => (
-          <tr key={circle.id}>
-            <td>
-              {circle.name}
-              {isJoined && <button>JOIN</button>}
-            </td>
+        {circlelist.map((circle) => {
+          // Determine whether to show the "JOIN" button for this circle
+          let shouldShowJoinButton = isJoined && id !== circle.owner_id;
+          return (
+            <tr key={circle.id}>
+              <td>
+                {circle.name}
+                {shouldShowJoinButton && <button>JOIN</button>}
+              </td>
 
-            <td>{circle.description}</td>
-          </tr>
-        ))}
+              <td>{circle.description}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
