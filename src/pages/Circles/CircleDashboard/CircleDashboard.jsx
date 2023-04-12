ff7493ebcb2ee5669bdc00e48bcf786bef09ed03
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-04-12 15:19:56 -0500

    added btns for share manuscripts, memebers, and msg board

--- src/pages/Circles/CircleDashboard/CircleDashboard.jsx
+++ src/pages/Circles/CircleDashboard/CircleDashboard.jsx
@@ -1,30 +1,43 @@
 import React, { useState } from "react";
 
 export default function CircleDashboard() {
-  
-  const handleSearch = () => {
-   
-  };
+  const handleSearch = () => {};
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
-     handleSearch();
+      handleSearch();
     }
   };
 
   return (
     <main className="content-main">
-      <h1>CircleDashboard</h1>
-      <input
-        type="text"
-        className="search-input"
-        placeholder="Search manuscript"
-        onChange={(e) => e.target.value}
-        onKeyPress={handleKeyPress}
-      />
-      <button className="search-btn" onClick={handleSearch}>
-        Search
-      </button>
+      <h1>Circle Dashboard</h1>
+      <h2>Circle Manuscripts</h2>
+      <div>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search manuscript"
+          onChange={(e) => e.target.value}
+          onKeyPress={handleKeyPress}
+        />
+        <button className="search-btn" onClick={handleSearch}>
+          Search
+        </button>
+        <h3>MANUSCRIPTS LIST</h3>
+      </div>
+
+      <div>
+        <button>Share Manuscript</button>
+      </div>
+
+      <div>
+        <button>Members</button>
+      </div>
+
+      <div>
+        <button>Message Board</button>
+      </div>
     </main>
   );
 }
