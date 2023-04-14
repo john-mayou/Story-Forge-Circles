f18d288b50d453839104586375de21943f98cb77
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-04-13 21:50:41 -0500

    refactored search circle page return statement

--- src/pages/Circles/SearchCirclesPage/SearchCirclesPage.jsx
+++ src/pages/Circles/SearchCirclesPage/SearchCirclesPage.jsx
@@ -3,41 +3,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useParams } from "react-router-dom";
 import CircleTableView from "../CircleTableView";
 import SearchCircleForm from "../SearchCircleForm";
+import CircleTableManuscriptView from "../CircleTableManuscriptView";
 
 export default function SearchCirclesPage() {
-  // Get the 'type' parameter from the URL
-  const { type } = useParams();
-  // Set up state for checking whether the data has been fetched or not
-  const [isDataFetched, setIsDataFetched] = useState(false);
-
-  // Set up Redux
   const dispatch = useDispatch();
-  const circleList = useSelector((store) => store.circles[type]);
-  const { id } = useSelector((store) => store.user);
-
-  // Get the search term from the URL query parameters
+  const { type } = useParams();
   const location = useLocation();
+  const { id } = useSelector((store) => store.user);
+  const circleList = useSelector((store) => store.circles[type]);
   const searchedTermFromQuery = new URLSearchParams(location.search).get(
     "term"
   );
 
-  // Set up state for the search term
   const [searchTerm, setSearchTerm] = useState(searchedTermFromQuery);
+  const hasSearchResults = circleList.length !== 0;
+  const isManuscriptList = type === "circleManuscriptsList";
+  const [isDataFetched, setIsDataFetched] = useState(false);
+  
+  // Filter the circle list based on the search term
+  const getFilteredCircles = () => {
+    let searchItem = "name";
+    if (type === "circleManuscriptsList") {
+      searchItem = "title";
+    }
+    return circleList?.filter((item) =>
+      searchTerm === ""
+        ? true
+        : item[searchItem].toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  };
 
-// Filter the circle list based on the search term
-const getFilteredCircles = () => {
-  let searchItem = "name";
-  if (type === "circleManuscriptsList") {
-    searchItem = "title";
-  }
-  return circleList?.filter((item) =>
-    searchTerm === ""
-      ? true
-      : item[searchItem].toLowerCase().includes(searchTerm.toLowerCase())
-  );
-};
-
-const filteredCircles = getFilteredCircles();
+  const filteredCircles = getFilteredCircles();
 
   // Fetch the circle data if it hasn't already been fetched
   useEffect(() => {
@@ -61,8 +57,8 @@ const filteredCircles = getFilteredCircles();
     }
   }, [type, circleList.length, id, isDataFetched]);
 
-   // Determine the type of the circle list based on the URL parameter.
-   const circleListType = () => {
+  // Determine the type of the circle list based on the URL parameter.
+  const circleListType = () => {
     switch (type) {
       case "myJoinedCircleList":
         return "Joined";
@@ -79,45 +75,24 @@ const filteredCircles = getFilteredCircles();
     setSearchTerm(searchTerm);
   };
 
-
-
   return (
     <main className="content-main">
       <h1>Search {circleListType()} Circles</h1>
-
       <SearchCircleForm onSearch={handleSearch} />
-
-      {/* Search results */}
       <h2>Search Results for "{searchTerm}"</h2>
-
-      {circleList.length === 0 ? <h2>No results found</h2> :
-      <>
-      {type !== "circleManuscriptsList" ? (
-        <CircleTableView
-          circlelist={filteredCircles}
-          isJoined={type === "allPublicCirclesList" ? true : false}
-        />
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Author</th>
-              <th>Title</th>
-              <th>Preview</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredCircles?.map((manuscript) => (
-              <tr key={manuscript?.id}>
-                <td>{manuscript?.author}</td>
-                <td>{manuscript?.title}</td>
-                <td>{manuscript?.body}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      {!hasSearchResults && <h2>No results found</h2>}
+      {hasSearchResults && (
+        <>
+          {!isManuscriptList ? (
+            <CircleTableView
+              circlelist={filteredCircles}
+              isJoined={type === "allPublicCirclesList"}
+            />
+          ) : (
+            <CircleTableManuscriptView manuscriptlist={filteredCircles} />
+          )}
+        </>
       )}
-      </>}
     </main>
   );
 }
