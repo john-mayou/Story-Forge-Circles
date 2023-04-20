8088749cafd13a0d44d069b868517eeceb364d5e
Connor Ferris <ferr0373@gmail.com>
2023-04-20 15:06:07 -0500

    resolved conflict so project doesn"t crash

--- src/pages/MessageBoardFiles/MessageBoardForm/MessageBoardForm.jsx
+++ src/pages/MessageBoardFiles/MessageBoardForm/MessageBoardForm.jsx
@@ -4,11 +4,7 @@ import { useHistory, useParams } from "react-router-dom";
 // font-awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReply } from "@fortawesome/free-solid-svg-icons";
-<<<<<<< HEAD
 import { TextField, Button } from "@mui/material";
-=======
-import { Button } from "@mui/material";
->>>>>>> main
 
 function MessageBoardForm({ parent_id, setReplyId }) {
   const { circle_id } = useParams();
